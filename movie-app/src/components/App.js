091f46b9dc07bab20118d902f2f2919b2ce1afc9
Zero <iamvamsi999@gmail.com>
2022-07-20 20:32:28 +0530

    Added actions and reducers for redux

--- movie-app/src/components/App.js
+++ movie-app/src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
 function App() {
-  return <div className="App">project set up</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tabs">Movies</div>
+          <div className="tabs">Favorites</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
