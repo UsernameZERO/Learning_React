3d5fce24c7d76fe93d5efd8f9b48c03e5f8aabbd
Zero <iamvamsi999@gmail.com>
2022-07-20 21:23:40 +0530

    created store for redux

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
 import "./index.css";
 import App from "./components/App";
+import movies from "./reducer";
+
+const store = configureStore({
+  reducer: {
+    movie: movies,
+  },
+});
+console.log("store", store);
+console.log("STATE", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
