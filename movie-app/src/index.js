cfffdd3b52d4bfed0f402c0b6aa991ea8427a6c1
Zero <iamvamsi999@gmail.com>
2022-07-21 11:41:13 +0530

    dispatch and subscribe in redux

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,21 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
+
 import "./index.css";
 import App from "./components/App";
 import movies from "./reducer";
 
 const store = configureStore({
   reducer: {
-    movie: movies,
+    movies: movies,
   },
 });
 console.log("store", store);
-console.log("STATE", store.getState());
+// console.log("Before STATE", store.getState());
+
+// store.dispatch({
+//   type: "ADD_MOVIES",
+//   movies: [{ name: "superman" }],
+// });
+
+// console.log("After STATE", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
